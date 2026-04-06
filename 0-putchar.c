0f5cd978d7983877a3fc47d542681dd97c1b3910
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-04-06 11:29:15 +0000

    Fix: use write instead of _putchar

--- 0-putchar.c
+++ 0-putchar.c
@@ -3,9 +3,9 @@
 /**
  * main - Entry point
  *
- * Description: prints "_putchar" using write
+ * Description: Prints "_putchar" followed by a new line
  *
- * Return: 0 on success
+ * Return: 0 (Success)
  */
 int main(void)
 {
