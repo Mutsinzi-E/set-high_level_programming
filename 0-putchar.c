376c5daa3ef8f2b1f7d36c856a67d4f5d2286812
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-04-06 11:15:51 +0000

    Task 0: _putchar program

--- 0-putchar.c
+++ 0-putchar.c
@@ -3,21 +3,21 @@
 /**
  * main - Entry point
  *
- * Description: prints _putchar
+ * Description: Program that prints _putchar
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-	return (0);
+return (0);
 }
