ea41b15589effc9aee77190228ba760ced7aea48
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-04-07 17:08:20 +0000

    Final Betty compliant version

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: Prints "_putchar" followed by a new line.
+ * Description: Prints "_putchar", followed by a new line.
  *
  * Return: Always 0 (Success)
  */
