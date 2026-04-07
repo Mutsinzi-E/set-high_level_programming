5b7b721713aaebd792d05a0aeedf21f544b49dc5
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-04-07 17:01:52 +0000

    Betty compliant fix for 0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,19 +3,21 @@
 /**
  * main - Entry point
  *
- * Description: Program that prints "_putchar" followed by a new line.
+ * Description: Prints "_putchar" followed by a new line.
  *
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char str[] = "_putchar\n";
-    int i;
-
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        _putchar(str[i]);
-    }
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
 
     return (0);
 }
