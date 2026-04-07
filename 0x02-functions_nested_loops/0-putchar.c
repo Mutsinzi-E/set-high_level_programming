bbc3bc2edc08c817d9f0a67b091aa15972997e1b
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-04-07 16:57:52 +0000

    Fix 0-putchar.c and meet Betty style

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: Program that prints "_putchar"
+ * Description: Program that prints "_putchar" followed by a new line.
  *
  * Return: 0 (Success)
  */
