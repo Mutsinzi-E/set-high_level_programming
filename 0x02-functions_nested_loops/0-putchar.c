26234e13366fe2a24e00ce1e95a536f99175ff10
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-04-07 16:35:01 +0000

    Fix Betty style for 0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+    char str[] = "_putchar\n";
+    int i;
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        _putchar(str[i]);
+    }
+
+    return (0);
+}
