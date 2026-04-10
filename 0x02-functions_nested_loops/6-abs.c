49fe4db795830de71acb420d00c144a7bd82b3ba
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-04-10 12:33:49 +0000

    Task 6: absolute value function

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value
+ * @n: integer
+ *
+ * Return: absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	return (n);
+}
