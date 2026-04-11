49c6bbf449115c5f90db43a18e5653217bf2bba1
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-04-11 14:08:21 +0000

    Complete pointers arrays strings project

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,10 +3,14 @@
 
 int _putchar(char c);
 
+/* string functions */
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
+char *string_toupper(char *);
+
+/* array function */
 void reverse_array(int *a, int n);
 
 #endif
