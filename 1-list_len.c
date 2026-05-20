ae421d6107473854eb6a46b2dadd7689e8dc9c2e
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-05-20 13:59:25 +0000

    Write a function that returns number of elements in a list_t list

--- 1-list_len.c
+++ 1-list_len.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list_t list
- * @h: pointer to the list
+ * list_len - returns number of elements in a linked list_t list
+ * @h: pointer to head of list
  *
  * Return: number of nodes
  */
@@ -10,7 +10,7 @@ size_t list_len(const list_t *h)
 {
 	size_t count = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		count++;
 		h = h->next;
