66302422efa77dbf6e9e753056fc9cff17d3d51b
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-05-21 10:28:42 +0000

    Fix list_len prototype

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -6,10 +6,8 @@
 /**
  * struct list_s - singly linked list
  * @str: string
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
+ * @len: length of string
+ * @next: points to next node
  */
 typedef struct list_s
 {
@@ -19,5 +17,6 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif
