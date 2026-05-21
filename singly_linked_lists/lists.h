62b697739135021b47c5f7701bea352510979c5a
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-05-21 10:42:32 +0000

    Write a function that adds a new node at the end of a list

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -7,7 +7,7 @@
  * struct list_s - singly linked list
  * @str: string
  * @len: length of string
- * @next: points to next node
+ * @next: pointer to next node
  */
 typedef struct list_s
 {
@@ -16,8 +16,10 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+/* prototypes */
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
+list_t *add_node(const list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
 
 #endif
