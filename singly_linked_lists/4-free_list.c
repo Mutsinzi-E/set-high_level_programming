09519088fe03e245c5388f87b9c29912c66ff9fd
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-05-21 11:43:38 +0000

    Free a singly linked list

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the first node of the list
+ *
+ * Return: nothing (void)
+ */
 void free_list(list_t *head)
 {
 	list_t *temp;
