8f1b134d4d86a7a5b2373a90b67cf1a3da56d8b0
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-05-22 11:43:27 +0000

    Add node at beginning of listint_t list

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -4,8 +4,8 @@
 #include <stddef.h>
 
 /**
- * struct listint_s - singly linked list node
- * @n: integer stored in the node
+ * struct listint_s - singly linked list
+ * @n: integer
  * @next: pointer to next node
  */
 typedef struct listint_s
@@ -14,7 +14,9 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* function prototype */
+/* REQUIRED PROTOTYPES */
+size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 
 #endif
