8f1617a1e8367932a2f6155769ec4cd43430e997
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-05-22 10:47:15 +0000

    Fix more singly linked lists project structure

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -3,13 +3,6 @@
 
 #include <stddef.h>
 
-/**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- */
 typedef struct listint_s
 {
 	int n;
@@ -17,5 +10,6 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 
 #endif
