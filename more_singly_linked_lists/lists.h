17c4dc9e36b02c635ac295bd63c85b6b67460dc1
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-05-22 11:12:41 +0000

    Complete listint_len task

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -3,13 +3,18 @@
 
 #include <stddef.h>
 
+/**
+ * struct listint_s - singly linked list node
+ * @n: integer stored in the node
+ * @next: pointer to next node
+ */
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
 
-size_t print_listint(const listint_t *h);
+/* function prototype */
 size_t listint_len(const listint_t *h);
 
 #endif
