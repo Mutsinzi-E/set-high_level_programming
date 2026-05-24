824c6c76bf41d06d6d790c454bdbbc8ea7d47b1a
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-05-24 12:48:31 +0000

    Implement safe list printing

--- more_singly_linked_lists/101-print_listint_safe.c
+++ more_singly_linked_lists/101-print_listint_safe.c
@@ -1,21 +1,20 @@
 #include "lists.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * print_listint_safe - prints a listint_t linked list safely
+ * print_listint_safe - prints list safely (handles loops)
  * @head: pointer to head
  *
  * Return: number of nodes printed
  */
 size_t print_listint_safe(const listint_t *head)
 {
-    size_t count = 0;
     const listint_t *current, *check;
+    size_t count = 0;
 
     current = head;
 
-    while (current != NULL)
+    while (current)
     {
         check = head;
 
