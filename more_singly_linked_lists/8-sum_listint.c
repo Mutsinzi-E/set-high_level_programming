2dfcd9dfb35cf820d0f0be6259dc0378c85f0490
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-05-24 12:17:02 +0000

    Fix Betty style for sum_listint

--- more_singly_linked_lists/8-sum_listint.c
+++ more_singly_linked_lists/8-sum_listint.c
@@ -1,21 +1,20 @@
 #include "lists.h"
 
 /**
- * sum_listint - returns the sum of all n values in a listint_t list
- * @head: pointer to the head of the list
+ * sum_listint - returns the sum of all data of a linked list
+ * @head: pointer to the first node
  *
- * Return: sum of all node data, or 0 if list is empty
+ * Return: sum of all the data, or 0 if list is empty
  */
 int sum_listint(listint_t *head)
 {
-    int sum = 0;
-    listint_t *current = head;
+	int sum = 0;
 
-    while (current != NULL)
-    {
-        sum += current->n;
-        current = current->next;
-    }
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
 
-    return (sum);
+	return (sum);
 }
