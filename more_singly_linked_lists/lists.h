44df73ab477068761f4ff5ed1df9508c9f0cad03
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-05-24 12:13:46 +0000

    Complete sum_listint linked list task

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -5,7 +5,7 @@
 
 /**
  * struct listint_s - singly linked list
- * @n: integer stored in the node
+ * @n: integer stored in node
  * @next: pointer to next node
  */
 typedef struct listint_s
@@ -14,7 +14,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* FUNCTIONS PROTOTYPES */
+/* FUNCTION PROTOTYPES */
 
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
@@ -26,7 +26,7 @@ void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
 
 int pop_listint(listint_t **head);
-
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int sum_listint(listint_t *head);
 
 #endif /* LISTS_H */
