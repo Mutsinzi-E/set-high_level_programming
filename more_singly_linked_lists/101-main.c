6bffd08f10407085622cf9ecb47c1e7412fd2f7c
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-05-24 14:50:35 +0000

    add missing main file

---
+++ more_singly_linked_lists/101-main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	listint_t *head;
+	listint_t *node;
+
+	head = NULL;
+
+	add_nodeint(&head, 0);
+	add_nodeint(&head, 1);
+	add_nodeint(&head, 2);
+	add_nodeint(&head, 3);
+	add_nodeint(&head, 4);
+	add_nodeint(&head, 98);
+	add_nodeint(&head, 402);
+	add_nodeint(&head, 1024);
+
+	print_listint_safe(head);
+
+	/* create a loop */
+	node = add_nodeint(&head, 0);
+	add_nodeint(&head, 1);
+	add_nodeint(&head, 2);
+
+	node->next = add_nodeint(&head, 98);
+
+	print_listint_safe(head);
+
+	return (0);
+}
