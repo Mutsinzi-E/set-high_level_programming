ebdd0f26cd8d4e892c0c675b1a439f2ad55c1379
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-05-26 13:53:18 +0000

    Fix safe linked list printing completely

--- more_singly_linked_lists/101-print_listint_safe.c
+++ more_singly_linked_lists/101-print_listint_safe.c
@@ -1,40 +1,93 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * print_listint_safe - prints a listint_t linked list safely
- * @head: pointer to the head of the list
+ * struct listp_s - linked list of pointers
+ * @p: pointer to node
+ * @next: points to next node
+ */
+typedef struct listp_s
+{
+	const void *p;
+	struct listp_s *next;
+} listp_t;
+
+/**
+ * free_listp - frees a listp_t list
+ * @head: pointer to head
+ */
+void free_listp(listp_t *head)
+{
+	listp_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
+
+/**
+ * add_nodep - adds new node address
+ * @head: pointer to head
+ * @p: pointer to save
+ *
+ * Return: address of new node or NULL
+ */
+listp_t *add_nodep(listp_t **head, const void *p)
+{
+	listp_t *new;
+
+	new = malloc(sizeof(listp_t));
+	if (!new)
+		exit(98);
+
+	new->p = p;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
+
+/**
+ * print_listint_safe - prints a listint_t list safely
+ * @head: pointer to list head
  *
  * Return: number of nodes printed
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current, *temp;
 	size_t count = 0;
+	listp_t *visited = NULL;
+	listp_t *check;
 
-	current = head;
-
-	while (current)
+	while (head)
 	{
-		temp = head;
+		check = visited;
 
-		while (temp != current)
+		while (check)
 		{
-			if (temp == current->next)
+			if (head == check->p)
 			{
 				printf("-> [%p] %d\n",
-					(void *)current->next,
-					current->next->n);
+					(void *)head, head->n);
+				free_listp(visited);
 				return (count);
 			}
-
-			temp = temp->next;
+			check = check->next;
 		}
 
-		printf("[%p] %d\n", (void *)current, current->n);
+		add_nodep(&visited, head);
+
+		printf("[%p] %d\n", (void *)head, head->n);
 		count++;
-		current = current->next;
+
+		head = head->next;
 	}
+
+	free_listp(visited);
 
 	return (count);
 }
