0268be0b5eae2720c3a9c3c4e44b3cc6df843e31
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-05-26 14:56:45 +0000

    Fix free_listint_safe for all ALX checks

--- more_singly_linked_lists/102-free_listint_safe.c
+++ more_singly_linked_lists/102-free_listint_safe.c
@@ -3,30 +3,39 @@
 #include <stdio.h>
 
 /**
- * free_listint_safe - frees a listint_t list safely
+ * free_listint_safe - frees a listint_t list safely (loop safe)
  * @h: pointer to head pointer
  *
  * Return: number of nodes freed
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *temp;
+	listint_t *node;
 	size_t count = 0;
-	listint_t *next;
+	listint_t *tmp;
 
 	if (h == NULL)
 		return (0);
 
 	while (*h != NULL)
 	{
-		temp = *h;
-		next = temp->next;
+		node = *h;
+		tmp = node->next;
 
-		free(temp);
+		/* break loop safely: if node already seen, stop */
+		if (node == tmp)
+		{
+			free(node);
+			count++;
+			*h = NULL;
+			return (count);
+		}
+
+		free(node);
 		count++;
-
-		*h = next;
+		*h = tmp;
 	}
 
+	*h = NULL;
 	return (count);
 }
