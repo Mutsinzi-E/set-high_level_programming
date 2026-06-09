3f977736d546cf35fdbf6eba8a6fa4d1471e9693
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-06-09 12:44:01 +0000

    Completed doubly linked list tasks 0-4

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -4,6 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct dlistint_s - doubly linked list node
+ * @n: integer
+ * @prev: pointer to previous node
+ * @next: pointer to next node
+ */
 typedef struct dlistint_s
 {
 	int n;
@@ -11,9 +17,19 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
+/* Task 0 */
 size_t print_dlistint(const dlistint_t *h);
+
+/* Task 1 */
 size_t dlistint_len(const dlistint_t *h);
+
+/* Task 2 */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+
+/* Task 3 */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+
+/* Task 4 */
+void free_dlistint(dlistint_t *head);
 
 #endif
