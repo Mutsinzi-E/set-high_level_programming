a1ee3a07fa2342bcc065f8a42fd15ec346a103fe
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-06-10 12:30:04 +0000

    Fix crackme5 keygen

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 /**
  * main - keygen for crackme5
@@ -11,8 +11,8 @@
 int main(int argc, char *argv[])
 {
 	char *u;
-	unsigned long int hash = 0;
 	int i;
+	unsigned long int sum = 0;
 	char key[7];
 
 	if (argc != 2)
@@ -24,10 +24,10 @@ int main(int argc, char *argv[])
 	u = argv[1];
 
 	for (i = 0; u[i]; i++)
-		hash += (u[i] * (i + 3));
+		sum += (u[i] * (i + 7));
 
 	for (i = 0; i < 6; i++)
-		key[i] = 33 + ((hash + i * 17) % 94);
+		key[i] = 33 + ((sum + i * 13) % 94);
 
 	key[6] = '\0';
 
