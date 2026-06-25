cc77d7778f88a3b9b325c95aa17c360b1d39edea
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-06-25 15:01:04 +0000

    Hash tables project completed and Betty fixed

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -3,60 +3,51 @@
 #include <string.h>
 
 /**
- * hash_table_set - adds or updates a key/value pair in a hash table
+ * hash_table_set - adds or updates key/value pair
  * @ht: hash table
- * @key: key (must not be empty)
- * @value: value (must be duplicated)
+ * @key: key string
+ * @value: value string
  *
  * Return: 1 on success, 0 on failure
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *tmp, *new_node;
+	unsigned long int i;
+	hash_node_t *tmp, *new;
 	char *k, *v;
 
 	if (!ht || !key || !*key || !value)
 		return (0);
 
-	index = key_index((const unsigned char *)key, ht->size);
-	tmp = ht->array[index];
+	i = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[i];
 
-	while (tmp)
-	{
-		if (strcmp(tmp->key, key) == 0)
-		{
-			v = strdup(value);
-			if (!v)
-				return (0);
-			free(tmp->value);
-			tmp->value = v;
-			return (1);
-		}
+	while (tmp && strcmp(tmp->key, key))
 		tmp = tmp->next;
+
+	if (tmp)
+	{
+		v = strdup(value);
+		if (!v)
+			return (0);
+		free(tmp->value);
+		tmp->value = v;
+		return (1);
 	}
 
-	new_node = malloc(sizeof(hash_node_t));
-	if (!new_node)
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
 		return (0);
 
 	k = strdup(key);
 	v = strdup(value);
-
 	if (!k || !v)
-	{
-		free(new_node);
-		if (k)
-			free(k);
-		if (v)
-			free(v);
-		return (0);
-	}
+		return (free(new), free(k), free(v), 0);
 
-	new_node->key = k;
-	new_node->value = v;
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
+	new->key = k;
+	new->value = v;
+	new->next = ht->array[i];
+	ht->array[i] = new;
 
 	return (1);
 }
