afbf160ee33ce5783991915a2c52fdd606a8a33c
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-06-26 12:06:43 +0000

    Fix sorted hash table implementation

--- hash_tables.h
+++ hash_tables.h
@@ -2,31 +2,41 @@
 #define HASH_TABLES_H
 
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 
 /**
- * struct hash_node_s - Node of a hash table
+ * struct shash_node_s - sorted hash node
  */
-typedef struct hash_node_s
+typedef struct shash_node_s
 {
-    char *key;
-    char *value;
-    struct hash_node_s *next;
-} hash_node_t;
+	char *key;
+	char *value;
+	struct shash_node_s *next;
+	struct shash_node_s *sprev;
+	struct shash_node_s *snext;
+} shash_node_t;
 
 /**
- * struct hash_table_s - Hash table structure
+ * struct shash_table_s - sorted hash table
  */
-typedef struct hash_table_s
+typedef struct shash_table_s
 {
-    unsigned long int size;
-    hash_node_t **array;
-} hash_table_t;
+	unsigned long int size;
+	shash_node_t **array;
+	shash_node_t *shead;
+	shash_node_t *stail;
+} shash_table_t;
 
-/* Function prototypes */
-hash_table_t *hash_table_create(unsigned long int size);
+/* REQUIRED FUNCTIONS */
+shash_table_t *shash_table_create(unsigned long int size);
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+char *shash_table_get(const shash_table_t *ht, const char *key);
+void shash_table_print(const shash_table_t *ht);
+void shash_table_print_rev(const shash_table_t *ht);
+void shash_table_delete(shash_table_t *ht);
+
+/* FROM PREVIOUS TASKS */
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
-int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 
 #endif
