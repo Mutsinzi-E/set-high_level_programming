ef0a90525829cc86c330b3277036c120764b8843
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-07-07 13:28:35 +0000

    Fix 8-concat_edges line count

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 word = "object-oriented"
 word2 = "programming"
-word3 = "with"
-word4 = "Python"
-print("{} {} {} {}".format(word, word2, word3, word4))
+word3 = "with Python"
+print("{} {} {}".format(word, word2, word3))
