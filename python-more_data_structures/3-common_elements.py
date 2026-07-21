4df456adbcb1ea1e77a8389f8b915d0cbda8b0d8
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-07-21 13:44:58 +0000

    Add common_elements function

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Function that returns common elements in two sets."""
+
+
+def common_elements(set_1, set_2):
+    """Return a set of elements common to set_1 and set_2."""
+    return set_1 & set_2
