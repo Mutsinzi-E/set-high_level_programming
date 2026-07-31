e123f13a83aa3fb1643ddb9a780225193da1e502
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-07-31 12:55:29 +0000

    Fix MyList sorted print return

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""Module that defines MyList class."""
+"""Module that defines the MyList class."""
 
 
 class MyList(list):
     """A class that inherits from list."""
 
     def print_sorted(self):
-        """Print the list sorted in ascending order."""
-        print(sorted(self))
+        """Print a sorted copy of the list and return it."""
+        sorted_list = sorted(self)
+        print(sorted_list)
+        return sorted_list
