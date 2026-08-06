f8235ed49c08a5c7d2a4898f766cc51461004f46
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-08-06 13:32:09 +0000

    Add log parsing statistics script

---
+++ python-input_output/101-stats.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Log parsing script."""
+
+import sys
+
+
+def print_stats(total_size, status_codes):
+    """Print statistics."""
+    print("File size: {}".format(total_size))
+
+    for code in sorted(status_codes):
+        print("{}: {}".format(code, status_codes[code]))
+
+
+total_size = 0
+status_codes = {}
+counter = 0
+
+try:
+    for line in sys.stdin:
+        parts = line.split()
+
+        if len(parts) < 7:
+            continue
+
+        try:
+            status = int(parts[-2])
+            size = int(parts[-1])
+
+            total_size += size
+
+            if status in [200, 301, 400, 401, 403, 404, 405, 500]:
+                if status not in status_codes:
+                    status_codes[status] = 0
+
+                status_codes[status] += 1
+
+            counter += 1
+
+            if counter == 10:
+                print_stats(total_size, status_codes)
+                counter = 0
+
+        except (ValueError, IndexError):
+            continue
+
+except KeyboardInterrupt:
+    print_stats(total_size, status_codes)
+    raise
