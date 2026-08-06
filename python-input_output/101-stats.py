07d1df52c112e6ab40edbaad4fee009bde0a3c20
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-08-06 13:40:32 +0000

    Fix wrong format handling in log parser

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -5,7 +5,7 @@ import sys
 
 
 def print_stats(total_size, status_codes):
-    """Print accumulated statistics."""
+    """Print statistics."""
     print("File size: {}".format(total_size))
 
     for code in sorted(status_codes):
@@ -14,37 +14,42 @@ def print_stats(total_size, status_codes):
 
 total_size = 0
 status_codes = {}
-lines_count = 0
+line_count = 0
+
+valid_codes = [200, 301, 400, 401, 403, 404, 405, 500]
 
 try:
     for line in sys.stdin:
         parts = line.split()
 
-        if len(parts) >= 2:
-            try:
-                status = int(parts[-2])
-                size = int(parts[-1])
+        if len(parts) < 7:
+            continue
 
-                total_size += size
+        try:
+            status = int(parts[-2])
+            size = int(parts[-1])
 
-                if status in [200, 301, 400, 401,
-                              403, 404, 405, 500]:
-                    if status not in status_codes:
-                        status_codes[status] = 0
-
-                    status_codes[status] += 1
-
-                lines_count += 1
-
-                if lines_count == 10:
-                    print_stats(total_size, status_codes)
-                    lines_count = 0
-
-            except ValueError:
+            if status not in valid_codes:
                 continue
+
+            total_size += size
+
+            if status not in status_codes:
+                status_codes[status] = 0
+
+            status_codes[status] += 1
+            line_count += 1
+
+            if line_count == 10:
+                print_stats(total_size, status_codes)
+                line_count = 0
+
+        except ValueError:
+            continue
 
 except KeyboardInterrupt:
     print_stats(total_size, status_codes)
     sys.exit(0)
 
-print_stats(total_size, status_codes)
+if line_count != 0:
+    print_stats(total_size, status_codes)
