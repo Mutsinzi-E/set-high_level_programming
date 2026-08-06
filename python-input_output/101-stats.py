cb6db42307310db27baef906c8603c4bcc1acfb4
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-08-06 13:37:29 +0000

    Add log parsing statistics

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -5,7 +5,7 @@ import sys
 
 
 def print_stats(total_size, status_codes):
-    """Print statistics."""
+    """Print accumulated statistics."""
     print("File size: {}".format(total_size))
 
     for code in sorted(status_codes):
@@ -14,36 +14,37 @@ def print_stats(total_size, status_codes):
 
 total_size = 0
 status_codes = {}
-counter = 0
+lines_count = 0
 
 try:
     for line in sys.stdin:
         parts = line.split()
 
-        if len(parts) < 7:
-            continue
+        if len(parts) >= 2:
+            try:
+                status = int(parts[-2])
+                size = int(parts[-1])
 
-        try:
-            status = int(parts[-2])
-            size = int(parts[-1])
+                total_size += size
 
-            total_size += size
+                if status in [200, 301, 400, 401,
+                              403, 404, 405, 500]:
+                    if status not in status_codes:
+                        status_codes[status] = 0
 
-            if status in [200, 301, 400, 401, 403, 404, 405, 500]:
-                if status not in status_codes:
-                    status_codes[status] = 0
+                    status_codes[status] += 1
 
-                status_codes[status] += 1
+                lines_count += 1
 
-            counter += 1
+                if lines_count == 10:
+                    print_stats(total_size, status_codes)
+                    lines_count = 0
 
-            if counter == 10:
-                print_stats(total_size, status_codes)
-                counter = 0
-
-        except (ValueError, IndexError):
-            continue
+            except ValueError:
+                continue
 
 except KeyboardInterrupt:
     print_stats(total_size, status_codes)
-    raise
+    sys.exit(0)
+
+print_stats(total_size, status_codes)
