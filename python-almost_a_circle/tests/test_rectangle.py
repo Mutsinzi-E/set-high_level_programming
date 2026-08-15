653baab13f56587fc260c1498f8e19a1fc19f555
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-08-15 14:34:45 +0000

    Add rectangle area method

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -170,3 +170,18 @@ class TestRectangle(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
+
+class TestRectangleArea(unittest.TestCase):
+    """Test Rectangle area."""
+
+    def test_area(self):
+        """Test area calculation."""
+        r1 = Rectangle(3, 2)
+        self.assertEqual(r1.area(), 6)
+
+        r2 = Rectangle(2, 10)
+        self.assertEqual(r2.area(), 20)
+
+        r3 = Rectangle(8, 7, 0, 0, 12)
+        self.assertEqual(r3.area(), 56)
