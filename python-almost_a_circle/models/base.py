d164af4bdcc0ab4935d00f11348da020f5b03b80
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-08-15 14:19:14 +0000

    Add Base class and tests

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Base class."""
-import json
+"""Defines the Base class."""
 
 
 class Base:
-    """Base class for all models."""
+    """Represents the base class for the project."""
 
     __nb_objects = 0
 
@@ -15,50 +14,3 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
-    @staticmethod
-    def to_json_string(list_dictionaries):
-        """Return JSON representation of a list of dictionaries."""
-        if list_dictionaries is None or list_dictionaries == []:
-            return "[]"
-        return json.dumps(list_dictionaries)
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """Write JSON representation of objects to a file."""
-        filename = cls.__name__ + ".json"
-        if list_objs is None:
-            list_objs = []
-        dictionaries = [obj.to_dictionary() for obj in list_objs]
-        with open(filename, "w", encoding="utf-8") as file:
-            file.write(cls.to_json_string(dictionaries))
-
-    @staticmethod
-    def from_json_string(json_string):
-        """Return a list from a JSON string."""
-        if json_string is None or json_string == "":
-            return []
-        return json.loads(json_string)
-
-    @classmethod
-    def create(cls, **dictionary):
-        """Create an instance with attributes from a dictionary."""
-        if cls.__name__ == "Rectangle":
-            instance = cls(1, 1)
-        elif cls.__name__ == "Square":
-            instance = cls(1)
-        else:
-            instance = cls(1)
-        instance.update(**dictionary)
-        return instance
-
-    @classmethod
-    def load_from_file(cls):
-        """Load objects from a JSON file."""
-        filename = cls.__name__ + ".json"
-        try:
-            with open(filename, "r", encoding="utf-8") as file:
-                dictionaries = cls.from_json_string(file.read())
-        except FileNotFoundError:
-            return []
-        return [cls.create(**dictionary) for dictionary in dictionaries]
--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,64 +1,46 @@
 #!/usr/bin/python3
-"""Tests for Base."""
+"""Unit tests for the Base class."""
+
 import unittest
+
 from models.base import Base
 
 
 class TestBase(unittest.TestCase):
-    """Test Base."""
+    """Test cases for Base."""
 
-    def test_auto_id(self):
-        """Test Base() assigns an ID automatically."""
-        first = Base()
-        self.assertIsInstance(first.id, int)
+    def test_id_automatic(self):
+        """Test automatic ID assignment."""
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b2.id, b1.id + 1)
 
-    def test_auto_id_increment(self):
-        """Test automatic IDs increment."""
-        first = Base()
-        second = Base()
-        self.assertEqual(second.id, first.id + 1)
+    def test_id_given(self):
+        """Test given ID."""
+        b = Base(89)
+        self.assertEqual(b.id, 89)
 
-    def test_explicit_id(self):
-        """Test Base(89) saves the passed ID."""
-        obj = Base(89)
-        self.assertEqual(obj.id, 89)
+    def test_id_given_does_not_increment(self):
+        """Test given ID does not affect automatic IDs."""
+        b1 = Base()
+        Base(89)
+        b2 = Base()
+        self.assertEqual(b2.id, b1.id + 1)
 
-    def test_to_json_none(self):
-        """Test to_json_string(None)."""
-        self.assertEqual(Base.to_json_string(None), "[]")
+    def test_id_zero(self):
+        """Test ID zero."""
+        b = Base(0)
+        self.assertEqual(b.id, 0)
 
-    def test_to_json_empty(self):
-        """Test to_json_string([])."""
-        self.assertEqual(Base.to_json_string([]), "[]")
+    def test_id_negative(self):
+        """Test negative ID."""
+        b = Base(-1)
+        self.assertEqual(b.id, -1)
 
-    def test_to_json_dictionary(self):
-        """Test JSON conversion of dictionaries."""
-        self.assertEqual(Base.to_json_string([{"id": 12}]), '[{"id": 12}]')
-
-    def test_to_json_returns_string(self):
-        """Test JSON result is a string."""
-        result = Base.to_json_string([{"id": 12}])
-        self.assertIsInstance(result, str)
-
-    def test_from_json_none(self):
-        """Test from_json_string(None)."""
-        self.assertEqual(Base.from_json_string(None), [])
-
-    def test_from_json_empty(self):
-        """Test from_json_string('[]')."""
-        self.assertEqual(Base.from_json_string("[]"), [])
-
-    def test_from_json_dictionary(self):
-        """Test from JSON dictionary."""
-        self.assertEqual(
-            Base.from_json_string('[{ "id": 89 }]'),
-            [{"id": 89}]
-        )
-
-    def test_from_json_returns_list(self):
-        """Test from_json_string returns a list."""
-        result = Base.from_json_string('[{ "id": 89 }]')
-        self.assertIsInstance(result, list)
+    def test_id_string(self):
+        """Test string ID."""
+        b = Base("test")
+        self.assertEqual(b.id, "test")
 
 
 if __name__ == "__main__":
