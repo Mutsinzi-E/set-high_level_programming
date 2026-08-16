7e47a97d4d40e7d3839b15b43acd87a8d70fabd3
Mutsinzi-E <mutsinzienock56@gmail.com>
2026-08-16 09:55:06 +0000

    Add Square dictionary representation

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Square class."""
+"""Defines the Square class."""
 
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square."""
+    """Represents a square."""
 
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize a Square."""
@@ -14,29 +14,36 @@ class Square(Rectangle):
     def __str__(self):
         """Return the string representation of the Square."""
         return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.width
+            self.id, self.x, self.y, self.size
         )
 
     @property
     def size(self):
-        """Return the size of the Square."""
+        """Get the size."""
         return self.width
 
     @size.setter
     def size(self, value):
-        """Set the size of the Square."""
+        """Set the size."""
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
-        """Update Square attributes using args or kwargs."""
-        attributes = ["id", "size", "x", "y"]
-
+        """Update Square attributes."""
         if args:
-            for index, value in enumerate(args):
-                if index < len(attributes):
-                    setattr(self, attributes[index], value)
+            attributes = ["id", "size", "x", "y"]
+            for i, value in enumerate(args):
+                setattr(self, attributes[i], value)
         else:
             for key, value in kwargs.items():
-                if key in attributes:
+                if key in ["id", "size", "x", "y"]:
                     setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Return the dictionary representation of a Square."""
+        return {
+            'id': self.id,
+            'size': self.size,
+            'x': self.x,
+            'y': self.y
+        }
